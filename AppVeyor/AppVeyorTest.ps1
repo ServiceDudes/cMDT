ef4eee2ad312cd98e29b511f0dd5b0a27dfec27e
Peter Eriksson <peter.ericsson@addlevel.se>
2017-01-20 10:32:53 +0100

    Added info to build/test process

--- AppVeyor/AppVeyorTest.ps1
+++ AppVeyor/AppVeyorTest.ps1
@@ -13,9 +13,12 @@ $results     = Invoke-Pester -Script $testFiles -OutputFormat NUnitXml -OutputFi
 
 Write-Host 'Uploading results'
 try {
+  Write-Host "About to upload file: $(Resolve-Path $resultsFile)"
   (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $resultsFile))
+  Write-Host 'Uploading complete' -ForegroundColor Green
 } catch {
   throw "Upload failed."
+  Write-Host 'Uploading failed!'  -ForegroundColor Red
 }
 
 #---------------------------------# 
