160b0441264a91af1aeff9f874ecf5615448ec8c
Marcus Kjell <marcus.kjell@addlevel.se>
2018-10-19 14:24:41 +0200

    Install & Uninstall Roles and Features for TS

--- src/DSCResources/cMDTTaskSequenceCustomize/cMDTTaskSequenceCustomize.psm1
+++ src/DSCResources/cMDTTaskSequenceCustomize/cMDTTaskSequenceCustomize.psm1
@@ -5,8 +5,6 @@ class cMDTTaskSequenceCustomize {
     [string]$TSFile
 
     [DscProperty(Mandatory)]
-    [parameter(Mandatory = $true)]
-    [ValidateSet("Run Command Line", "Restart Computer", "Install Application", "Set Task Sequence Variable")]
     [string]$Type
 
     [DscProperty(Key)]
@@ -54,6 +52,18 @@ class cMDTTaskSequenceCustomize {
     [DscProperty()]
     [string]$PSDrivePath
 
+    [DscProperty()]
+    [string]$OSName
+
+    [DscProperty()]
+    [string]$OSRoles
+
+    [DscProperty()]
+    [string]$OSRoleServices
+
+    [DscProperty()]
+    [string]$OSFeatures
+
     [void] Set() {
         $ts = $this.ReadTaskSequenceXML()
 
@@ -65,19 +75,54 @@ class cMDTTaskSequenceCustomize {
 
         $parentGroup = $ts.SelectSingleNode($query)
 
+        $oSIndex = @{
+            "Windows 7"            = 4
+            "Windows 2008 R2"      = 5
+            "Windows 2008 R2 Core" = 6
+            "Windows 8"            = 7
+            "Windows 2012"         = 8
+            "Windows 2012 Core"    = 9
+            "Windows 8.1"          = 10
+            "Windows 2012 R2"      = 11
+            "Windows 2012 R2 Core" = 12
+            "Windows 10"           = 13
+            "Windows 2016"         = 14
+            "Windows 2016 Core"    = 15
+        }
+
+        $tsType = @{
+            RunCommandLine            = "Run Command Line"
+            RestartComputer           = "Restart Computer"
+            InstallApplication        = "Install Application"
+            SetTaskSequenceVariable   = "Set Task Sequence Variable"
+            InstallRolesAndFeatures   = "Install Roles and Features"
+            UninstallRolesAndFeatures = "Uninstall Roles and Features"
+        }
+
         # Create new step
         switch ($this.Type) {
-            "Run Command Line" {
+            $tsType.RunCommandLine {
                 $this.RunCommandLine($ts, $parentGroup)
             }
-            "Restart Computer" {
+            $tsType.RestartComputer {
                 $this.RestartComputer($ts, $parentGroup)
             }
-            "Install Application" {
+            $tsType.InstallApplication {
                 $this.AddApplication($ts, $parentGroup)
             }
-            "Set Task Sequence Variable" {
+            $tsType.SetTaskSequenceVariable {
                 $this.SetTaskSequenceVariable($ts, $parentGroup)
+            }
+            $tsType.InstallRolesAndFeatures {
+                $this.ValidateOsName($oSIndex)
+                $this.InstallRolesAndFeatures($ts, $parentGroup, $oSIndex)
+            }
+            $tsType.UninstallRolesAndFeatures {
+                $this.ValidateOsName($oSIndex)
+                $this.UninstallRolesAndFeatures($ts, $parentGroup, $oSIndex)
+            }
+            default { 
+                throw "$($this.Type) is not a known Type. `nValid values: $($tsType.Values)" 
             }
         }
 
@@ -110,6 +155,23 @@ class cMDTTaskSequenceCustomize {
         }
 
         return $xml
+    }
+
+    [void] AddStepToTaskSequence($step, $parentGroup) {
+        if ($this.InsertAfterStep) {
+            $insertAfterNode = $parentGroup.SelectSingleNode("step[@name='$($this.InsertAfterStep)']")
+
+            $parentGroup.InsertAfter($step, $insertAfterNode)
+        }
+        else {
+            $parentGroup.AppendChild($step)
+        }
+    }
+
+    [void] ValidateOsName($oSIndex) {
+        if (-not ($oSIndex.$($this.OSName))) {
+            throw "$($this.OSName) is not a known OS name! `nValid values: $($oSIndex.Keys)"
+        }
     }
 
     [void] RunCommandLine($ts, $parentGroup) {
@@ -157,13 +219,7 @@ class cMDTTaskSequenceCustomize {
         $step.AppendChild($action)
         $step.AppendChild($varList)
 
-        if($this.InsertAfterStep) {
-            $insertAfterNode = $parentGroup.SelectSingleNode("step[@name='$($this.InsertAfterStep)']")
-
-            $parentGroup.InsertAfter($step,$insertAfterNode)
-        } else {
-            $parentGroup.AppendChild($step)
-        }
+        $this.AddStepToTaskSequence($step, $parentGroup)
     }
 
     [void] RestartComputer($ts, $parentGroup) {
@@ -204,13 +260,7 @@ class cMDTTaskSequenceCustomize {
         $step.AppendChild($action)
         $step.AppendChild($varList)
 
-        if($this.InsertAfterStep) {
-            $insertAfterNode = $parentGroup.SelectSingleNode("step[@name='$($this.InsertAfterStep)']")
-
-            $parentGroup.InsertAfter($step,$insertAfterNode)
-        } else {
-            $parentGroup.AppendChild($step)
-        }
+        $this.AddStepToTaskSequence($step, $parentGroup)
     }
 
     [void] AddApplication($ts, $parentGroup) {
@@ -263,13 +313,7 @@ class cMDTTaskSequenceCustomize {
         $step.AppendChild($action)
         $step.AppendChild($varList)
 
-        if($this.InsertAfterStep) {
-            $insertAfterNode = $parentGroup.SelectSingleNode("step[@name='$($this.InsertAfterStep)']")
-
-            $parentGroup.InsertAfter($step,$insertAfterNode)
-        } else {
-            $parentGroup.AppendChild($step)
-        }
+        $this.AddStepToTaskSequence($step, $parentGroup)
     }
 
     [void] SetTaskSequenceVariable($ts, $parentGroup) {
@@ -309,12 +353,108 @@ class cMDTTaskSequenceCustomize {
         $step.AppendChild($action)
         $step.AppendChild($varList)
 
-        if($this.InsertAfterStep) {
-            $insertAfterNode = $parentGroup.SelectSingleNode("step[@name='$($this.InsertAfterStep)']")
+        $this.AddStepToTaskSequence($step, $parentGroup)
+    }
 
-            $parentGroup.InsertAfter($step,$insertAfterNode)
-        } else {
-            $parentGroup.AppendChild($step)
-        }
+    [void] InstallRolesAndFeatures($ts, $parentGroup, $oSIndex) {
+        $step = $ts.CreateElement("step")
+
+        $step.SetAttribute("name", $this.Name)
+        $step.SetAttribute("description", $this.Description)
+
+        $step.SetAttribute("type", "BDD_InstallRoles")
+        $step.SetAttribute("disable", "false")
+        $step.SetAttribute("continueOnError", "false")
+        $step.SetAttribute("successCodeList", $this.SuccessCodeList)
+        
+        $step.SetAttribute("runIn", "WinPEandFullOS")
+                                                
+        $varList = $ts.CreateElement("defaultVarList")
+
+        $index = $ts.CreateElement("variable")
+        $index.SetAttribute("name", "OSRoleIndex")
+        $index.SetAttribute("property", "OSRoleIndex")
+        $index.AppendChild($ts.CreateTextNode($oSIndex.$($this.OSName)))
+        $varList.AppendChild($index)
+
+        $roles = $ts.CreateElement("variable")
+        $roles.SetAttribute("name", "OSRoles")
+        $roles.SetAttribute("property", "OSRoles")
+        $roles.AppendChild($ts.CreateTextNode($this.OSRoles))
+        $varList.AppendChild($roles)
+
+        $services = $ts.CreateElement("variable")
+        $services.SetAttribute("name", "OSRoleServices")
+        $services.SetAttribute("property", "OSRoleServices")
+        $services.AppendChild($ts.CreateTextNode($this.OSRoleServices))
+        $varList.AppendChild($services)
+
+        $features = $ts.CreateElement("variable")
+        $features.SetAttribute("name", "OSFeatures")
+        $features.SetAttribute("property", "OSFeatures")
+        $features.AppendChild($ts.CreateTextNode($this.OSFeatures))
+        $varList.AppendChild($features)
+
+        $action = $ts.CreateElement("action")
+        $action.AppendChild($ts.CreateTextNode('cscript.exe "%SCRIPTROOT%\ZTIOSRole.wsf"'))
+
+        $step.AppendChild($varList)
+        $step.AppendChild($action)
+
+        $this.AddStepToTaskSequence($step, $parentGroup)
+    }
+  
+    [void] UninstallRolesAndFeatures($ts, $parentGroup, $oSIndex) {
+        $step = $ts.CreateElement("step")
+
+        $step.SetAttribute("name", $this.Name)
+        $step.SetAttribute("description", $this.Description)
+
+        $step.SetAttribute("type", "BDD_UninstallRoles")
+        $step.SetAttribute("disable", "false")
+        $step.SetAttribute("continueOnError", "false")
+        $step.SetAttribute("successCodeList", $this.SuccessCodeList)
+        
+        $step.SetAttribute("runIn", "WinPEandFullOS")
+                                                
+        $varList = $ts.CreateElement("defaultVarList")
+
+        $index = $ts.CreateElement("variable")
+        $index.SetAttribute("name", "OSRoleIndex")
+        $index.SetAttribute("property", "OSRoleIndex")
+        $index.AppendChild($ts.CreateTextNode($oSIndex.$($this.OSName)))
+        $varList.AppendChild($index)
+
+        $remove = $ts.CreateElement("variable")
+        $remove.SetAttribute("name", "CompletelyRemove")
+        $remove.SetAttribute("property", "CompletelyRemove")
+        $remove.AppendChild($ts.CreateTextNode('false'))
+        $varList.AppendChild($remove)
+
+        $roles = $ts.CreateElement("variable")
+        $roles.SetAttribute("name", "UninstallOSRoles")
+        $roles.SetAttribute("property", "UninstallOSRoles")
+        $roles.AppendChild($ts.CreateTextNode($this.OSRoles))
+        $varList.AppendChild($roles)
+
+        $services = $ts.CreateElement("variable")
+        $services.SetAttribute("name", "UninstallOSRoleServices")
+        $services.SetAttribute("property", "UninstallOSRoleServices")
+        $services.AppendChild($ts.CreateTextNode($this.OSRoleServices))
+        $varList.AppendChild($services)
+
+        $features = $ts.CreateElement("variable")
+        $features.SetAttribute("name", "UninstallOSFeatures")
+        $features.SetAttribute("property", "UninstallOSFeatures")
+        $features.AppendChild($ts.CreateTextNode($this.OSFeatures))
+        $varList.AppendChild($features)
+
+        $action = $ts.CreateElement("action")
+        $action.AppendChild($ts.CreateTextNode('cscript.exe "%SCRIPTROOT%\ZTIOSRole.wsf" /uninstall'))
+
+        $step.AppendChild($varList)
+        $step.AppendChild($action)
+
+        $this.AddStepToTaskSequence($step, $parentGroup)
     }
 }
